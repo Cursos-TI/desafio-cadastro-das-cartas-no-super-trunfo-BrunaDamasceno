d2f1a1f34a8151509b523a3eca01dd54702b5ab0
BrunaDamasceno <brunasilva0597@gmail.com>
2025-04-03 21:10:16 +0000

    NOVO commit

---
+++ supertrunfobruna.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    printf ("Desafio Super Trunfo!\n");
+    int populacao = 70000;
+    float area = 70.000;
+    float pib = 7.000;
+    int pontos = 70;
+    char nome[20] = "AMAZONAS";
+    
+    
+    printf("populacao: %d habitantes\n", populacao);
+    printf("area: %f quadrados\n", area);
+    printf("pib: %f da cidade\n", pib);
+    printf("pontos: %d quantidade\n", pontos);
+    printf("nome: %s\n", nome);
+    
+    
+    return 0;
+    
+    
+    
+    }
+    
